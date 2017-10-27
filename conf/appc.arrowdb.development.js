2a68e4d5fe4a16fa20541d88ab769672ca13aa7b
wolfet410 <wolfet410@gmail.com>
2017-10-27 11:13:17 -0400

    Init

---
+++ conf/appc.arrowdb.development.js
@@ -0,0 +1,16 @@
+/**
+ * Generated ArrowDB configuration for development
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "development",
+      "key": "J64W99WsJYlRKnXSUMDGHLY1Tg8JXj6Z",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user_dev",
+      "password": "3Li3i1iwmBtjaQpuep"
+    }
+  }
+};
---
+++ conf/appc.arrowdb.production.js
@@ -0,0 +1,16 @@
+/**
+ * Generated ArrowDB configuration for production
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "production",
+      "key": "gsffTIoHeQ7DwCswraHJxEf7dqE4T7De",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user",
+      "password": "FN3scgrnh5PVEMN4vu"
+    }
+  }
+};
